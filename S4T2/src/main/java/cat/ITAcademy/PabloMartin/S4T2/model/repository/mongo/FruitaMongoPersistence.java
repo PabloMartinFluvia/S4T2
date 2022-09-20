10c1d4fb3cd899366e6da78dca5c163636c1606a
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2022-09-20 11:55:23 +0200

    Create FruitaMongoPersistence.java

---
+++ S4T2/src/main/java/cat/ITAcademy/PabloMartin/S4T2/model/repository/mongo/FruitaMongoPersistence.java
@@ -0,0 +1,56 @@
+package cat.ITAcademy.PabloMartin.S4T2.model.repository.mongo;
+
+import cat.ITAcademy.PabloMartin.S4T2.model.domain.Fruita;
+import cat.ITAcademy.PabloMartin.S4T2.model.repository.FruitaPersistence;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository(value = "mongo")
+public class FruitaMongoPersistence implements FruitaPersistence{
+
+    @Autowired
+    private FruitaMongoRepository fruitaMongoRepository;
+    
+    @Override
+    public Fruita add(Fruita fruita) {
+        return fruitaMongoRepository.save(new FruitaEntity(fruita)).toFruita();
+    }
+
+    @Override
+    public Optional<Fruita> findByNom(String nom) {
+        return fruitaMongoRepository.findByNom(nom).map(FruitaEntity::toFruita);
+    }
+
+    @Override
+    public boolean existById(int id) {
+        return fruitaMongoRepository.existsById(id);
+    }
+
+    @Override
+    public Fruita update(Fruita fruita) {
+        return add(fruita);
+    }
+
+    @Override
+    public void deleteOne(int id) {
+        fruitaMongoRepository.deleteById(id);        
+    }
+
+    @Override
+    public Optional<Fruita> getOne(int id) {
+        return fruitaMongoRepository.findById(id).map(FruitaEntity::toFruita);
+    }
+
+    @Override
+    public List<Fruita> getAll() {
+        List<Fruita> fruites = new LinkedList<>();
+        fruitaMongoRepository.findAll().forEach(fruitaEntity -> {
+            fruites.add(fruitaEntity.toFruita());
+        });
+        return fruites;
+    }
+    
+}
