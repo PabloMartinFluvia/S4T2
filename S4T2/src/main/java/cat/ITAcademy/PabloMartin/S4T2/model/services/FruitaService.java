34e38b8f966f3841c31d753c88b5e79140f6950d
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2022-09-20 11:00:21 +0200

    updated conflict in update end point

--- S4T2/src/main/java/cat/ITAcademy/PabloMartin/S4T2/model/services/FruitaService.java
+++ S4T2/src/main/java/cat/ITAcademy/PabloMartin/S4T2/model/services/FruitaService.java
@@ -12,57 +12,62 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class FruitaService {
-    
+
     //per fer la prova d'injectar un altre adaptador de persistencia
     //  <-> provar de tenir dao jpa i dao mongo en un mateix projecte
-    @Resource(name = "${bd.option}") 
+    @Resource(name = "${bd.option}")
     private FruitaPersistence fruitaPersistence;
-    
-    public Fruita add(Fruita fruita){
+
+    public Fruita add(Fruita fruita) {
         /*
         Si està proporcionat un nom que ja existeix en la BD
             -> error 409 CONFLICT -> ConflictException            
-        */
+         */
         assertNomNotExist(fruita.getNom());
         return fruitaPersistence.add(fruita);
     }
-    
+
     public void assertNomNotExist(String nom) {
-        fruitaPersistence.findByNom(nom)             
-            .ifPresent(fruita -> {
-                throw new ConflictException
-                    ("Existeix un altre registre amb aquest nom (i no pot ser duplicat): " + nom);
-            });
+        fruitaPersistence.findByNom(nom)
+                .ifPresent(fruita -> {
+                    throw new ConflictException("Existeix un altre registre amb aquest nom (i no pot ser duplicat): " + nom);
+                });
     }
-    
-    public Fruita update(Fruita fruita){
+
+    public Fruita update(Fruita fruita) {
         int id = fruita.getId();
-        if(fruitaPersistence.existById(id)){
+        if (fruitaPersistence.existById(id)) {
             /*
             Si està proporcionat un nom que ja existeix en la BD en un altre registre
             -> error 409 CONFLICT -> ConflictException            
-            */
-            assertNomNotExist(fruita.getNom());
+             */
+            assertNomNotInOther(fruita.getNom(),id);
             return fruitaPersistence.update(fruita);
-        }else{
-            throw new NotFoundException
-                ("No hi ha registrat cap fruita amb el id proporcionat: "+id);
+        } else {
+            throw new NotFoundException("No hi ha registrat cap fruita amb el id proporcionat: " + id);
         }
     }
-    
-    public void deleteOne(int id){
-        if(fruitaPersistence.existById(id)){ // to avoid EmptyResultDataAccessException
+
+    public void assertNomNotInOther(String nom, int id) {
+        fruitaPersistence.findByNom(nom).ifPresent(fruita -> {
+            if (fruita.getId() != id) {
+                throw new ConflictException("Existeix un altre registre amb aquest nom (i no pot ser duplicat): " + nom);
+            }
+        });
+    }
+
+    public void deleteOne(int id) {
+        if (fruitaPersistence.existById(id)) { // to avoid EmptyResultDataAccessException
             fruitaPersistence.deleteOne(id);
-        }        
+        }
     }
-        
-    public Fruita getOne(int id){        
+
+    public Fruita getOne(int id) {
         return fruitaPersistence.getOne(id).orElseThrow(
-            ()-> new NotFoundException
-                ("No hi ha registrat cap fruita amb el id proporcionat: "+id)
-            );        
+                () -> new NotFoundException("No hi ha registrat cap fruita amb el id proporcionat: " + id)
+        );
     }
-    
+
     public List<Fruita> getAll() {
         //TO DO ==>> Cal fer algu si no hi ha cap registre / document guardat?
         return fruitaPersistence.getAll();
