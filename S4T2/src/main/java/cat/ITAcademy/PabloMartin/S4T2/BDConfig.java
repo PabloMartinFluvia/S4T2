f2651d12c604721641f5ec91fb5dc208aefb8e39
PabloMartinFluvia <102052827+PabloMartinFluvia@users.noreply.github.com>
2022-09-20 14:33:47 +0200

    Update BDConfig.java

--- S4T2/src/main/java/cat/ITAcademy/PabloMartin/S4T2/BDConfig.java
+++ S4T2/src/main/java/cat/ITAcademy/PabloMartin/S4T2/BDConfig.java
@@ -1,12 +1,18 @@
 package cat.ITAcademy.PabloMartin.S4T2;
 
 import cat.ITAcademy.PabloMartin.S4T2.model.repository.FruitaPersistence;
+import cat.ITAcademy.PabloMartin.S4T2.model.repository.jpa.FruitaJpaPersistence;
 import cat.ITAcademy.PabloMartin.S4T2.model.repository.mongo.FruitaMongoPersistence;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BDConfig {
+    
+    @Bean(name = "jpa")    
+    public FruitaPersistence fruitaJpaPersistence(){
+        return new FruitaJpaPersistence();
+    }
     
     @Bean(name = "mon")    
     public FruitaPersistence fruitaMongoPersistence(){
